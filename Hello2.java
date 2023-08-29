0f71c3f0d2f2d1a83a64e0bba71a9b6c16e84780
DHoward188 <143538025+DHoward188@users.noreply.github.com>
2023-08-29 13:31:04 -0400

    first commit

---
+++ Hello2.java
@@ -0,0 +1,13 @@
+// Filename Hello2.java 
+// Written by Dylan Howard
+// Written on 8/24/23
+
+public class Hello2
+{
+/* This class demonstrates the use of the println() method to print the message Hello, world! */
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ HelloDialog.java
@@ -0,0 +1,11 @@
+// Filename HelloDialog.java 
+// Written by Dylan Howard
+// Written on 8/24/23
+import javax.swing.JOptionPane; 
+public class HelloDialog 
+{
+    public static void main(String[] args) 
+    {
+        JOptionPane.showMessageDialog(null, "Hello, world!"); 
+    }     
+}
